cdf72174104e4eb048bbe930e746b7d1e6cca4b1
fjunqueira <fabio.blumm18@gmail.com>
2026-02-05 16:42:58 -0300

    add tenant migration to schema

--- scripts/builder-entrypoint.sh
+++ scripts/builder-entrypoint.sh
@@ -4,4 +4,9 @@ cd apps/builder;
 node  -e "const { configureRuntimeEnv } = require('next-runtime-env/build/configure'); configureRuntimeEnv();"
 cd ../..;
 
+if [ "$NODE_ENV" != "production" ]; then
+    echo "Running migrations..."
+    ./node_modules/.bin/prisma migrate deploy --schema=packages/prisma/postgresql/schema.prisma
+fi
+
 HOSTNAME=0.0.0.0 PORT=3000 node apps/builder/server.js;
