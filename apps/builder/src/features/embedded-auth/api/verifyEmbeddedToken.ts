18842ab2e0aaad9e90eb4a8400d5da160137a31b
MeBSilva <matheusfull@live.com>
2026-02-27 17:33:58 -0300

    :construction: improved error handling for verify embedded token procedure

--- apps/builder/src/features/embedded-auth/api/verifyEmbeddedToken.ts
+++ apps/builder/src/features/embedded-auth/api/verifyEmbeddedToken.ts
@@ -2,14 +2,22 @@ import { publicProcedure } from '@/helpers/server/trpc'
 import { verifyCognitoToken } from '@/features/auth/helpers/verifyCognitoToken'
 import { env } from '@typebot.io/env'
 import { z } from 'zod'
+import { TRPCError } from '@trpc/server'
 
 export const verifyEmbeddedToken = publicProcedure
   .input(z.object({ token: z.string() }))
   .mutation(async ({ input }) => {
-    const payload = await verifyCognitoToken({
-      cognitoAppClientId: env.CLOUDCHAT_COGNITO_APP_CLIENT_ID,
-      cognitoIssuerUrl: env.COGNITO_ISSUER_URL,
-      cognitoToken: input.token,
-    })
-    return { email: payload.email }
+    try {
+      const payload = await verifyCognitoToken({
+        cognitoAppClientId: env.CLOUDCHAT_COGNITO_APP_CLIENT_ID,
+        cognitoIssuerUrl: env.COGNITO_ISSUER_URL,
+        cognitoToken: input.token,
+      })
+      return { email: payload.email }
+    } catch (error) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'Invalid or expired token',
+      })
+    }
   })
